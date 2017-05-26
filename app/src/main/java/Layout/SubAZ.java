a018cbddd1d265988a1838c0b3f7a9b895ffc4f2
mnjrupp <mnjrupp@hotmail.com>
2017-05-26 07:30:12 -0500

    Updated code

--- app/src/main/java/Layout/SubAZ.java
+++ app/src/main/java/Layout/SubAZ.java
@@ -1,11 +1,14 @@
 package Layout;
 
+import android.support.annotation.StringDef;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.mnjru.tabbed_1828_dictionary.R;
 
@@ -14,11 +17,18 @@ import com.example.mnjru.tabbed_1828_dictionary.R;
  */
 
 public class SubAZ  extends Fragment{
+    ListView listView;
+    ArrayAdapter<String> listAdapter;
+    private String[] items = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
+    "Q","R","S","T","U","V","W","X","Y","Z"};
     public SubAZ(){}
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_az,container,false);
+        listView = (ListView) view.findViewById(R.id.list_az);
+        listAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, items);
+        listView.setAdapter(listAdapter);
         return view;
     }
 }
