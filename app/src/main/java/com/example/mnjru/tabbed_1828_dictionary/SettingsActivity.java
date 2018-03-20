1a11a2554d7d02f0c9eb5c875642bf866adba3a8
mnjrupp <mnjrupp@hotmail.com>
2018-03-19 23:43:42 -0500

    Multiple changes to code.

--- app/src/main/java/com/example/mnjru/tabbed_1828_dictionary/SettingsActivity.java
+++ app/src/main/java/com/example/mnjru/tabbed_1828_dictionary/SettingsActivity.java
@@ -3,12 +3,7 @@ package com.example.mnjru.tabbed_1828_dictionary;
 
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
@@ -17,14 +12,11 @@ import android.preference.PreferenceActivity;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
 import java.util.List;
 
-import Utilities.ThemeUtility;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -101,7 +93,13 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.preferences_container);
         setupActionBar();
+        GeneralPreferenceFragment generalPreferenceFragment = new GeneralPreferenceFragment();
+        getFragmentManager().beginTransaction()
+                .add(R.id.preferences, generalPreferenceFragment)
+                .commit();
+
     }
 
 
@@ -142,7 +140,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     @Override
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public void onBuildHeaders(List<Header> target) {
-        loadHeadersFromResource(R.xml.pref_headers, target);
+       // loadHeadersFromResource(R.xml.pref_headers, target);
     }
 
     /**
@@ -150,18 +148,24 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
      * Make sure to deny any unknown fragments here.
      */
     protected boolean isValidFragment(String fragmentName) {
-        return PreferenceFragment.class.getName().equals(fragmentName)
-                || GeneralPreferenceFragment.class.getName().equals(fragmentName);
+        return true;
+                //PreferenceFragment.class.getName().equals(fragmentName)
+               // || GeneralPreferenceFragment.class.getName().equals(fragmentName);
                 //|| DataSyncPreferenceFragment.class.getName().equals(fragmentName)
                 //|| NotificationPreferenceFragment.class.getName().equals(fragmentName);
     }
 
-    /**
-     * This fragment shows general preferences only. It is used when the
-     * activity is showing a two-pane settings UI.
-     */
+
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class GeneralPreferenceFragment extends PreferenceFragment {
+
+      /*  public static GeneralPreferenceFragment newInstance()
+        {
+            GeneralPreferenceFragment myFragment = new GeneralPreferenceFragment();
+            Bundle bundle = new Bundle();
+            myFragment.setArguments(bundle);
+            return myFragment;
+        }*/
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -180,7 +184,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
             if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
+               // startActivity(new Intent(getActivity(), SettingsActivity.class));
                 return true;
             }
             return super.onOptionsItemSelected(item);
